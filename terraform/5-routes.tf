a75af40ccd485f33d409049cc8c574a91587a3fe
KingMicky <you@example.com>
2024-01-11 15:16:15 +0100

    Update route table associations

--- terraform/5-routes.tf
+++ terraform/5-routes.tf
@@ -20,6 +20,26 @@ resource "aws_route_table" "private" {
 ]
 
   tags = {
-    Name = "private"
+    Name = "public"
   }
+}
+
+resource "aws_route_table_association" "private-us-east-1a" {
+  subnet_id      = aws_subnet.private-us-east-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-us-east-1b" {
+  subnet_id      = aws_subnet.private-us-east-1b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public-us-east-1a" {
+  subnet_id      = aws_subnet.private-us-east-1c.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public-us-east-1b" {
+  subnet_id      = aws_subnet.private-us-east-1b.id
+  route_table_id = aws_route_table.private.id
 }
